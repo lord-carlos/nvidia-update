07375eb82f2847d67e7278a7f36aafb0e7d1dfe9
Teoxoy <28601907+Teoxoy@users.noreply.github.com>
2018-11-15 01:31:03 +0100

    more formatting

--- nvidia.ps1
+++ nvidia.ps1
@@ -5,8 +5,8 @@
 
 # Verify user has elevated permissions
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-	Exit
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
 }
 
 
@@ -35,21 +35,21 @@ if (Test-Path $env:programfiles\7-zip\7z.exe) {
     Write-Host "Please install 7zip or WinRAR."
     Write-Host "Press any key to exit..."
     $key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-	exit
+    exit
 }
 
 
 # Checking currently installed driver version
 Write-Host "Attempting to detect currently installed driver version..."
 try {
-    $ins_version = (Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.devicename -like "*nvidia*" -and $_.devicename -notlike "*audio*"}).DriverVersion.SubString(7).Remove(1,1).Insert(3,".")
+    $ins_version = (Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.devicename -like "*nvidia*" -and $_.devicename -notlike "*audio*"}).DriverVersion.SubString(7).Remove(1, 1).Insert(3, ".")
 } catch {
     Write-Host "Unable to detect a compatible Nvidia device."
     Write-Host "Press any key to exit..."
     $key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-	exit
+    exit
 }
-    Write-Host "Installed version `t$ins_version"
+Write-Host "Installed version `t$ins_version"
 
 # Checking latest driver version from Nvidia website
 $link = Invoke-WebRequest -Uri 'https://www.nvidia.com/Download/processFind.aspx?psid=101&pfid=816&osid=57&lid=1&whql=1&lang=en-us&ctk=0' -Method GET -UseBasicParsing
@@ -60,10 +60,10 @@ Write-Host "Latest version `t`t$version"
 
 # Comparing installed driver version to latest driver version from Nvidia
 if ($version -eq $ins_version) {
-	Write-Host "The installed version is the same as the latest version."
+    Write-Host "The installed version is the same as the latest version."
     Write-Host "Press any key to exit..."
     $key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-	exit
+    exit
 }
 
 
@@ -76,18 +76,18 @@ if (!$secondaryDrive) {
 
 
 # Checking Windows version
-if ([Environment]::OSVersion.Version -ge (new-object 'Version' 9,1)) {
-	$windowsVersion = "win10"
+if ([Environment]::OSVersion.Version -ge (new-object 'Version' 9, 1)) {
+    $windowsVersion = "win10"
 } else {
-	$windowsVersion = "win8-win7"
+    $windowsVersion = "win8-win7"
 }
 
 
 # Checking Windows bitness
 if ((Get-WmiObject win32_operatingsystem | Select-Object osarchitecture).osarchitecture -eq "64-bit") {
-	$windowsArchitecture = "64bit"
+    $windowsArchitecture = "64bit"
 } else {
-	$windowsArchitecture = "32bit"
+    $windowsArchitecture = "32bit"
 }
 
 
@@ -113,7 +113,7 @@ if ($archiverProgram -eq "$env:programfiles\7-zip\7z.exe") {
 Write-Host "Installing Nvidia drivers now..."
 $install_args = "-s -noreboot -noeula"
 if ($cleanInstall) {
-	$install_args = $install_args + " -clean"
+    $install_args = $install_args + " -clean"
 }
 Start-Process -FilePath "$extractDir\$version\setup.exe" -ArgumentList $install_args -wait
 
@@ -126,7 +126,7 @@ if ($scheduleTask -ne $FALSE) {
     $taskname = "Nvidia-Updater"
     $description = "Update Your Driver!"
     $action = New-ScheduledTaskAction -Execute "C:\Task\Nvidia.ps1"
-    $trigger =  New-ScheduledTaskTrigger -Weekly -WeeksInterval $scheduleTask -DaysOfWeek $scheduleDay -At $scheduleTime
+    $trigger = New-ScheduledTaskTrigger -Weekly -WeeksInterval $scheduleTask -DaysOfWeek $scheduleDay -At $scheduleTime
     Register-ScheduledTask -TaskName $taskname -Action $action -Trigger $trigger -Description $description 2>&1 | Out-Null
 }
 
